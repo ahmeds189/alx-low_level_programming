37ac3a793eb3cc90454e9a0f4fc34edc82ad118d
ahmed <primaryahmedsaid@gmail.com>
2023-03-21 23:38:31 +0200

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,20 +8,38 @@
  */
 void print_times_table(int n)
 {
-int i, j;
+	int num, mult, prod;
 
-if (n > 15 || n < 0)
-return;
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
 
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-if (j == 0)
-_putchar("%d", i * j);
-else
-_putchar(", %d", i * j);
-}
-_putchar("\n");
-}
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
