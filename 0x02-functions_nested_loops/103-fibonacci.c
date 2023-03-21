d052b444acf93391c63850d346ae563814abc267
ahmed <primaryahmedsaid@gmail.com>
2023-03-21 23:48:34 +0200

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms in the Fibonacci sequence whose values do not exceed 4,000,000
+ * main -  finds and prints the sum of the even-valued terms,followed by a new line.
  *
  * Return: 0
  */
