95702464c5fb14baecc40f7e9e6c5800a6030e97
ahmed <primaryahmedsaid@gmail.com>
2023-03-24 20:50:21 +0200

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_inline.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal.
+ * @n: numbers of time the character to be printed
+ */
+void print_line(int n)
+{
+if(n <= 0)
+{
+_putchar('\n');
+return;
+}
+else{
+for (int i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
