89fd2dbb97e46deed4f7951bef3338b21b230688
ahmed <primaryahmedsaid@gmail.com>
2023-03-30 07:51:21 +0200

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,42 +1,85 @@
 #include "main.h"
 
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 /**
- * infinite_add - Adds two numbers
- * @n1: First number to add
- * @n2: Second number to add
- * @r: Buffer to store result
- * @size_r: Size of buffer
+ * add_strings - Adds the numbers stored in two strings.
+ * @n1: The string containing the first number to be added.
+ * @n2: The string containing the second number to be added.
+ * @r: The buffer to store the result.
+ * @r_index: The current index of the buffer.
  *
- * Return: Pointer to result, or 0 if result cannot be stored in buffer
+ * Return: If r can store the sum - a pointer to the result.
+ *         If r cannot store the sum - 0.
+ */
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+
+	else if (tens && r_index < 0)
+		return (0);
+
+	return (r + r_index + 1);
+}
+
+
+
+
+/**
+ * infinite_add - Adds two numbers.
+ * @n1: The first number to be added.
+ * @n2: The second number to be added.
+ * @r: The buffer to store the result.
+ * @size_r: The buffer size.
+ *
+ * Return: If r can store the sum - a pointer to the result.
+ *         If r cannot store the sum - 0.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, l1, l2, sum;
-char tmp;
+	int index, n1_len = 0, n2_len = 0;
 
-for (l1 = 0; n1[l1]; l1++)
-;
-for (l2 = 0; n2[l2]; l2++)
-;
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
 
-if (l1 > size_r || l2 > size_r)
-return (0);
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
 
-for (i = l1 - 1, j = l2 - 1, k = 0, sum = 0; i >= 0 || j >= 0 || sum; i--, j--, k++)
-{
-sum += (i >= 0 ? n1[i] - '0' : 0) + (j >= 0 ? n2[j] - '0' : 0);
-r[k] = (sum % 10) + '0';
-sum /= 10;
-}
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
 
-r[k] = '\0';
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
 
-for (i = 0, j = k - 1; i < j; i++, j--)
-{
-tmp = r[i];
-r[i] = r[j];
-r[j] = tmp;
-}
-
-return (r);
+	return (add_strings(n1, n2, r, --size_r));
 }
