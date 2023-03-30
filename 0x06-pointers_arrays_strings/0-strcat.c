2ff282b2bca29fd07f8ad47fc92b4e217e274533
ahmed <primaryahmedsaid@gmail.com>
2023-03-30 07:00:46 +0200

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int i, j;
+  int index = 0, dest_len = 0;
 
-  for (i = 0; dest[i] != '\0'; i++)
-    ;
+	while (dest[index++])
+		dest_len++;
 
-  for (j = 0; src[j] != '\0'; j++)
-    dest[i + j] = src[j];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-  dest[i + j] = '\0';
-
-  return (dest);
+	return (dest);
 }
