84036f07a7b3d84ced8f89367c0a9806839445cb
ahmed <primaryahmedsaid@gmail.com>
2023-03-30 08:04:25 +0200

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,43 +1,54 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * infinite_add - Adds two numbers
- * @n1: First number
- * @n2: Second number
- * @r: Buffer that the function will use to store the result
- * @size_r: Buffer size
- *
- * Return: Pointer to the result, or 0 if the result can not be stored in r
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-  int i = 0, j = 0, k = 0, l = 0, m = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-  while (n1[i] != '\0')
-    i++;
-  while (n2[j] != '\0')
-    j++;
-  if (i > size_r || j > size_r)
-    return (0);
-  j--;
-  i--;
-  while (i >= 0 || j >= 0 || k)
-  {
-    if (i >= 0)
-      l += n1[i--] - '0';
-    if (j >= 0)
-      l += n2[j--] - '0';
-    if (l >= 10)
-      k = 1;
-    else
-      k = 0;
-    r[m++] = (l % 10) + '0';
-    l /= 10;
-  }
-  if (m > size_r)
-    return (0);
-  r[m] = '\0';
-  for (i = 0; i < m / 2; i++)
-    k = r[i], r[i] = r[m - i - 1], r[m - i - 1] = k;
-  return (r);
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
+		return (0);
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
+	{
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
+	}
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
