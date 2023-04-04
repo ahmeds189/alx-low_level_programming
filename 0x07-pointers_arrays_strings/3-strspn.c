abb0344f2b55c8d925cbc6454a56a38ed9f35c05
ahmed <primaryahmedsaid@gmail.com>
2023-04-04 07:46:17 +0200

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring
- * @s: pointer to string
- * @accept: pointer to substring
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
  *
- * Return: number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: The number of bytes in s which
+ *         consist only of bytes from accept.
  */
 unsigned int _strspn(char *s, char *accept)
 {
