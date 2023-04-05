7cac86f34a8f2fcbc5320add76a517349f650393
ahmed <primaryahmedsaid@gmail.com>
2023-04-05 16:50:47 +0200

    finish

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,45 @@
 #include "main.h"
+
+/**
+ * is_palindrome - returns 1 if string is palindrome, otherwise 0
+ * @s: string to check for palindromicity
+ *
+ * Return: 1 if s is palindrome, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	if (len <= 1)
+		return (1);
+	return (palindrome_helper(s, len));
+}
+
+/**
+ * palindrome_helper - helper function for is_palindrome
+ * @s: string to check for palindromicity
+ * @len: length of s
+ *
+ * Return: 1 if s is palindrome, otherwise 0
+ */
+int palindrome_helper(char *s, int len)
+{
+	if (*s != *(s + len - 1))
+		return (0);
+	if (len <= 1)
+		return (1);
+	return (palindrome_helper(s + 1, len - 2));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate length of
+ *
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,1 +1,25 @@
 #include "main.h"
+
+/**
+ * wildcmp - compares two strings and returns 1 if they can be considered identical
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 1 if s1 and s2 can be considered identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	if (*s1 == '\0')
+		return (*s2 == '\0');
+	if (*s1 != *s2)
+		return (0);
+	return (wildcmp(s1 + 1, s2 + 1));
+}
