f2835039f1d6969c03b9f1e7e640da0c6ef633bf
ahmed <primaryahmedsaid@gmail.com>
2023-04-05 14:58:31 +0200

    starter files

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stddef.h>
 
 int _putchar(char c);
@@ -13,6 +12,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 
 #endif
