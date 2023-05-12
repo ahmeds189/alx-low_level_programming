4a62e31d58fc2b42001b2bdcf22baa972cfcabd4
ahmed <primaryahmedsaid@gmail.com>
2023-05-12 13:29:14 -0700

    fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int open, write, len = 0;
+	int _open, _write, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,13 +21,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write = write(open, text_content, len);
+	_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	_write = write(open, text_content, len);
 
-	if (open == -1 || write == -1)
+	if (_open == -1 || _write == -1)
 		return (-1);
 
-	close(open);
+	close(_open);
 
 	return (1);
 }
